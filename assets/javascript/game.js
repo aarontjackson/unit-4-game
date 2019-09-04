9697e368849306e3301924a0abe11b67675db75b
Aaron Jackson <aarontjackson@outlook.com>
2019-09-04 16:08:33 -0400

    adding logic to the 4 crystals that can be selected using a switch statement

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,50 @@
 $(document).ready(function () {
 // game interface script
+let wins = 0;
+let losses = 0;
+let totalScore = 0;
+
+// Game Method
+
 let gameManager = {
+    setGameStart: function(crystalType) {
+        this.resetGame(crystalType);
+        this.setTotal();
+    },
+    resetGame: function(crystalType) {
+        switch (crystalType) {
+            case "Diamond":
+            crystal = new Crystal(crystalType, randomAmount);
+            break;
+            case "Dragonstone":
+            crystal = new Crystal(crystalType, randomAmount);
+            break;
+            case "Topaz":
+            crystal = new Crystal(crystalType, randomAmount);
+            break;
+            case "Lapis":
+            crystal = new Crystal(crystalType, randomAmount);
+            break;
+            default:
+                alert("Select a crystal until you've reached the number");
+        }
+    },
+    setTotal: function() {
 
-
+    }
+  
 }
-//gem selection script
-let gem;
 
-function Gem(gemType, randomAmount) {
-    this.gemType = gemType;
-    this.randomAmount = amount;
+// Crystal Method
+
+//crystal object is globally avaliable
+let Crystal;
+//crystal object will generate a random number 1 - 12
+function Crystal(crystalType, randomAmount) {
+    this.crystalType = crystalType;
+    this.randomAmount = Math.floor((Math.random() * 12) + 1);
 }
+
+   
 
 });
