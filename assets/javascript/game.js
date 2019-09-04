3562a5d43d03a0c7151445ffb4babef0f7260f13
Aaron Jackson <aarontjackson@outlook.com>
2019-09-04 14:39:01 -0400

    creating game interface in javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+// game interface script
+let gameManager = {
+
+
+}
+//gem selection script
+let gem;
+
+function Gem(gemType, randomAmount) {
+    this.gemType = gemType;
+    this.randomAmount = amount;
+}
+
+});
