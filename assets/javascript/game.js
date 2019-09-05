e935bccc236469018be057a353f6e6205dafb87a
Aaron Jackson <aarontjackson@outlook.com>
2019-09-04 21:04:14 -0400

    added J Query reference link

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,7 @@
 // $( document ).ready(function() {
 //     console.log("ready!");
 
-    // game interface script
+    // Global variables
     let wins = 0;
     let losses = 0;
     let totalScore = 0;
@@ -10,30 +10,55 @@
 
     // Game Method
     function gameManager() {
+
+        $("#crystal").empty();
+
         randomResult = Math.floor((Math.random() * 120) + 19);
-    }
-    $(".recorder").text(randomResult);
+   
+        $(".recorder").text(randomResult);
 
-    // Crystal Method
-    for (let c = 0; c < 4; c++){
-        let random = Math.floor((Math.random() * 12) + 1);  
-        return crystal = random;
+    
+         for (let c = 0; c < 4; c++){
+
+            let random = Math.floor((Math.random() * 12) + 1);  
+
+            return crystal = random;
+
+        $('#crystal').append(crystal); 
     } 
-    $('#crystal').append(crystal); 
+}
+gameManager();
 
+    //Crystal Event Method
     $(document).on("click", "#crystal", function () {
+
         let result = parseInt($(this));
+
         totalScore += result;
 
+        $(".gemCounter").text("Total Score: " + totalScore);
+
         if(totalScore > randomResult) {
-            console.log("You lost!");
+
             losses++;
+
             $("#losses").text(losses);
+
             totalScore = 0;
+
+            gameManager();
+
         }
         else if(totalScore === randomResult){
+
             win++;
+
             $("#wins").text(losses);
+
             totalScore = 0;
+
+            gameManager();
         }
+
+        gameManager();
     });
